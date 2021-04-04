660bec83911f9eba0797b7879bbebe78e72ce313
kamikoto00 <kebkal.danil@gmail.com>
2021-04-04 19:20:13 +0300

    test version 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 const path = require('path');
-const webpack = require('webpack');
 
-module.exports = {
+module.exports = (env, options) => ({
   entry: './src/index.ts',
-  devtool: 'source-map',
+  devtool: /*options.mode === "production" ? false : */'source-map',
   module: {
     rules: [
       {
@@ -22,11 +21,11 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
-    port: 80,
+    port: 8080,
   },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: []
-};
+});
