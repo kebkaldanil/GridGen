7a8addaad4665eaac05f75cd51ee221d9bd7e949
kamikoto00 <kebkal.danil@gmail.com>
2021-04-08 03:01:47 +0300

    Version 3. Changed... I am just too lazy

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { forSaveOrLoad, Grid, Random } from './helpers';
+const imgElementRegexp = /<img.+src="(.+?)".*>/;
 const images = [] as HTMLImageElement[];
 const output_image = $<HTMLCanvasElement>("#output_image");
 const images_container = $("#images_container");
@@ -21,33 +22,16 @@ const settingsS = (() => {
         size4: $("#size4")
     };
 })();
-settingsE.slideUp(0);
-$("#settings_show").on("click", settingsE.slideDown.bind(settingsE));
-$("#settings_hide").on("click", () => {
-    settingsE.slideUp();
-    forSaveOrLoad<JQuery, string | number>(settingsS, settings, (el, obj, key) => {
-        let val = el[key].val() as string | number;
-        if (Number.isInteger(val))
-            val = + val;
-        obj[key] = val;
-    });
-    localStorage.setItem("settings", JSON.stringify(settings));
-    init();
-    generate();
-});
 const ctx = output_image[0].getContext("2d");
-let random: Random;
-let grid: Grid;
-
 const settings = Object.assign({
     seed: "",
     grid: {
-        width: 5,
-        height: 5
+        width: 16,
+        height: 9
     },
     image: {
-        width: 300,
-        height: 300
+        width: 1920,
+        height: 1080
     },
     size1: 1,
     size2: 1,
@@ -59,13 +43,19 @@ $("#images_zone")
         e.preventDefault();
         e.stopPropagation();
     }).on("drop", async (ev) => {
-        const nimages = Array.from(ev.originalEvent.dataTransfer.files)
-            .filter(b => b.type.startsWith("image"))
-            .map(img => URL.createObjectURL(img))
-            .concat(await Promise.all(Array.from(ev.originalEvent.dataTransfer.items)
-            .filter(i => i.type === "text/uri-list")
-            .map(i => new Promise<string>(r => i.getAsString(r)))))
-            .map(uri => $<HTMLImageElement>(`<img class="w-100" src="${uri}" />`)[0]);
+        const uris = (await Promise.all(Array.from(ev.originalEvent.dataTransfer.items)
+            .filter(i => i.type === "text/uri-list" || i.type === "text/html")
+            .map(i => {
+                const type = i.type;
+                return new Promise<string>(r => i.getAsString(str => {
+                if (type === "text/html") {
+                    const rr = imgElementRegexp.exec(str);
+                    r(typeof rr === "object" ? rr[1] : null);
+                } else
+                    r(str);
+            }))
+        }))).filter(v => v);
+        const nimages = uris.map(uri => $<HTMLImageElement>(`<img class="w-100 src_img" src="${uri}" />`)[0]);
         Promise.all(nimages.map(v => new Promise<void>(resolve => $(v).on("load", () => {
             const ni = nimages.filter(v => {
                 const isLoaded = v.complete && v.naturalHeight !== 0;
@@ -73,32 +63,48 @@ $("#images_zone")
                     v.remove();
                 return isLoaded;
             });
-            images_container.append(ni);
+            $(ni).appendTo(images_container);
             images.push(...ni);
-            //images.push(...nimages);
             resolve();
         })))).then(generate);
     });
-init();
+
+output_image.attr({
+    width: settings.image.width,
+    height: settings.image.height
+});
+
+forSaveOrLoad<JQuery, string | number>(settingsS, settings, (el, obj, key) => el[key].val(obj[key]));
+
+forSaveOrLoad(settingsS, settings, (el, obj, key) => el[key].on("input", () => {
+    forSaveOrLoad(settingsS, settings, (el, obj, key) => obj[key] = el[key].val() as string);
+    localStorage.setItem("settings", JSON.stringify(settings));
+    output_image.attr({
+        width: settings.image.width,
+        height: settings.image.height
+    });
+    generate();
+}));
 
 function placeImage(cis: CanvasImageSource, x: number, y: number, w: number, h: number) {
-    const pw = settings.image.width / grid.width;
-    const ph = settings.image.height / grid.height
+    const pw = settings.image.width / settings.grid.width;
+    const ph = settings.image.height / settings.grid.height
     ctx.drawImage(cis, x * pw, y * ph, pw * w, ph * h);
 }
 
 function generate() {
+    let random = new Random(settings.seed === "" ? null : settings.seed);
     ctx.clearRect(0, 0, settings.image.width, settings.image.height);
-    grid.clearSquere(0, 0, grid.width, grid.height);
+    const grid = new Grid(settings.grid.width, settings.grid.height);
     const sizes = [{
         size: 1,
-        chance: settings.size1
+        chance: +settings.size1
     }, {
         size: 2,
-        chance: settings.size2
+        chance: +settings.size2
     }, {
         size: 4,
-        chance: settings.size4
+        chance: +settings.size4
     }];
     //const chanceSum = sizes.reduce((p, c) => p + c.chance, 0);
     //sizes.forEach(v => v.chance /= chanceSum);
@@ -119,19 +125,7 @@ function generate() {
     for (let i = 0; i < grid.length; ++i)
         if (!grid.get(i)) {
             grid.set(true, i);
-            const {x, y} = grid.getXY(i);
+            const { x, y } = grid.getXY(i);
             placeImage(random.from(images), x, y, 1, 1);
         }
-}
-
-function init() {
-    output_image.attr({
-        width: settings.image.width,
-        height: settings.image.height
-    });
-
-    forSaveOrLoad<JQuery, string | number>(settingsS, settings, (el, obj, key) => el[key].val(obj[key]));
-
-    grid = new Grid(settings.grid.width, settings.grid.height);
-    random = new Random(settings.seed === "" ? null : settings.seed);
 }
